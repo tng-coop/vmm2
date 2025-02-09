fa9682e1a43455f3e334a20921e532ce90d065cf
Yasuaki Kudo <yasu@tng.coop>
2025-02-10 02:27:34 +0900

    a

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,17 @@
 import { defineConfig } from "vite";
+import path from "path";
 
 export default defineConfig({
   base: "./", // for a user site, this is correct
   build: {
     sourcemap: true,
+  },
+  resolve: {
+    alias: {
+      // Force all imports of 'lit' to resolve to the one in our root node_modules
+      'lit': path.resolve(__dirname, 'node_modules/lit'),
+    },
+    dedupe: ['lit']
   },
   optimizeDeps: {
     exclude: [
