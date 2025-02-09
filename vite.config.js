8f116356b7a13e8baf8b29ad54ff8daae00134d9
Yasuaki Kudo <yasu@tng.coop>
2025-02-09 16:29:46 +0900

    f

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: './', // for a user site, this is correct
+  base: "./", // for a user site, this is correct
   build: {
-    sourcemap: false 
-  }
+    sourcemap: true,
+  },
 });
